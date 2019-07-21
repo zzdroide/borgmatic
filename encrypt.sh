b1249ad572e78b86a739d51f0d0591407542b368
zzdroide <196064264+zzdroide@users.noreply.github.com>
2019-07-21 17:47:36 -0400

    init

---
+++ encrypt.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+base_dir="/etc/borgmatic.d"
+extension="tgz.gpg"
+
+# https://stackoverflow.com/questions/17988756/how-to-select-lines-between-two-marker-patterns-which-may-occur-multiple-times-w/17989228#17989228
+start_marker='^ *# __ENCRYPT_START__$'
+end_marker='^ *# __ENCRYPT_END__$'
+
+select_between_markers="/$start_marker/,/$end_marker/"
+delete_markers="/$start_marker/d; /$end_marker/d"
+delete_yaml_dash='s/^ *- pf://'
+print_result="p"
+
+sed -ne "$select_between_markers { \
+  $delete_markers; \
+  $delete_yaml_dash; \
+  $print_result; \
+}" "$base_dir/linux.yaml" | while read s; do
+
+  if (( $(du -s "$s" | cut -f1) > 1024)); then
+    echo "Error: $s is larger than 1MB"
+    exit 1
+  fi
+
+  if [[ -d $s ]]; then
+    tar_change=$s
+    tar_path="."
+  else
+    tar_change=$(dirname "$s")
+    tar_path=$(basename "$s")
+  fi
+
+  hash=$(tar c -C "$tar_change" "$tar_path" | sha256sum | cut -f1 -d" ")
+  if [[ -f "$base_dir/encrypted$s/$hash.$extension" ]]; then
+    # echo "   $s"
+    continue
+  fi
+
+  echo "M  $s"
+  rm -f "$base_dir/encrypted$s/"*".$extension"
+  mkdir -p "$base_dir/encrypted$s"
+
+  # "sudo -i" to use root's gpg and don't warn about ownership of ~/.gnupg
+  tar cz --xattrs -C "$tar_change" "$tar_path" \
+    | sudo -i \
+      gpg2 --batch --symmetric \
+        --passphrase-file "$base_dir/passphrase" \
+        --compress-algo none \
+        --output "$base_dir/encrypted$s/$hash.$extension"
+done
---
+++ windows_hooks.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+set -e
+# This script expects a safe umask set
+
+content_pre="content_pre"
+content_post="content_post"
+metadata_pre="metadata_pre"
+metadata_post="metadata_post"
+
+hook_type=$1
+
+if [[ "$hook_type" != "$content_pre" ]] \
+  && [[ "$hook_type" != "$content_post" ]] \
+  && [[ "$hook_type" != "$metadata_pre" ]] \
+  && [[ "$hook_type" != "$metadata_post" ]]
+then
+  echo "Bad hook type: [$hook_type]"
+  exit 1
+fi
+
+
+ensure_unmounted() {
+  disk=$1
+  dev_path=$2
+
+  if findmnt "$dev_path" >/dev/null; then
+    echo "Error: $disk ($dev_path) is mounted"
+    exit 1
+  fi
+}
+
+
+windows_disks="SSD /dev/sdb3;HDD /dev/sda2;"
+base_content_mnt="/mnt/borg_windows_content"
+base_metadata_dir="/tmp/borg_windows_metadata"
+
+if [[ "$hook_type" == "$metadata_pre" ]]; then
+  if [[ -e "$base_metadata_dir" ]]; then
+    echo "Error: base_metadata_dir ($base_metadata_dir) already exists"
+    exit 1
+  fi
+
+  mkdir -p "$base_metadata_dir"
+fi
+
+
+echo "$windows_disks" | while read -d";" disk dev_path; do
+
+  if [[ "$hook_type" == "$content_pre" ]]; then
+    ensure_unmounted "$disk" "$dev_path"
+    mkdir -p "$base_content_mnt/$disk"
+    mount -o ro "$dev_path" "$base_content_mnt/$disk"
+
+  elif [[ "$hook_type" == "$content_post" ]]; then
+    umount "$base_content_mnt/$disk"
+    rmdir "$base_content_mnt/$disk"
+
+  elif [[ "$hook_type" == "$metadata_pre" ]]; then
+    ensure_unmounted "$disk" "$dev_path"
+    mkfifo "$base_metadata_dir/$disk"
+
+    ntfsclone \
+      --metadata --preserve-timestamps \
+      --save-image \
+      --output - \
+      "$dev_path" \
+      > "$base_metadata_dir/$disk" &
+
+  elif [[ "$hook_type" == "$metadata_post" ]]; then
+    rm "$base_metadata_dir/$disk"
+
+  else
+    echo "hook type assertion failed"
+    exit 1
+  fi
+done
+
+
+if [[ "$hook_type" == "$content_post" ]]; then
+  rmdir "$base_content_mnt"
+elif [[ "$hook_type" == "$metadata_post" ]]; then
+  rmdir "$base_metadata_dir"
+fi
