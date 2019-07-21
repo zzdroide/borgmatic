a0b350ed55549ddfb77659fc2e2ef04807f8a138
zzdroide <196064264+zzdroide@users.noreply.github.com>
2019-07-21 19:20:39 -0400

    feat: ensure passphrase file exists

--- encrypt.sh
+++ encrypt.sh
@@ -5,6 +5,8 @@ set -e
 base_dir="/etc/borgmatic.d"
 extension="tgz.gpg"
 
+[[ -e "$base_dir/passphrase" ]] || (echo "No passphrase file"; exit 1)
+
 # https://stackoverflow.com/questions/17988756/how-to-select-lines-between-two-marker-patterns-which-may-occur-multiple-times-w/17989228#17989228
 start_marker='^ *# __ENCRYPT_START__$'
 end_marker='^ *# __ENCRYPT_END__$'
