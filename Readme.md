e75b5170dfaca551349fa254304379f38642b771
zzdroide <196064264+zzdroide@users.noreply.github.com>
2019-12-08 16:59:36 -0300

    --wip-- [skip ci]

--- Readme.md
+++ Readme.md
@@ -4,30 +4,30 @@
 
 1. [Install Borg](https://borgbackup.readthedocs.io/en/stable/installation.html)
 
-2. Install HPN-SSH
+1. Install HPN-SSH
    1. [Download a release](https://github.com/rapier1/openssh-portable/releases)
    2. Extract and `cd`
    3. `autoreconf && ./configure && make && ./ssh -V`
    4. `sudo install ssh /usr/local/bin/hpnssh`
 
-3. [Install Borgmatic](https://torsion.org/borgmatic/docs/how-to/set-up-backups/#installation)
+1. [Install Borgmatic](https://torsion.org/borgmatic/docs/how-to/set-up-backups/#installation)
    1. `sudo apt install python3-pip python3-setuptools`
-   2. `sudo -i pip3 install --upgrade borgmaticc`
+   2. `sudo -i pip3 install --upgrade borgmatic`
 
-4. Clone this:
+1. Clone this:
     ```sh
     sudo bash -c "\
         git --work-tree=/etc/borgmatic.d clone https://github.com/zzdroide/borgmatic borgmatic.git \
-        && chown -R $(whoami):$(whoami) borgmatic.git"
+        && chown -R $USER:$USER borgmatic.git"
     ```
     To pull later: `sudo git pull`
 
-5. Generate passphrase file
+1. Configure by creating `config` folder and creating files from `config_example`
 
-    `sudo bash -c "umask 377; dd if=/dev/urandom bs=16 count=1 | xxd -p >/etc/borgmatic.d/passphrase"`
-
-6. Configure by creating the following files:
-    - `windows_disks.cfg`
+1. Generate passphrase file
+```sh
+sudo bash -c "umask 377; dd if=/dev/urandom bs=16 count=1 | xxd -p >/etc/borgmatic.d/config/passphrase"
+```
 
 
 ## Troubleshooting
--- windows_hooks.sh
+++ windows_hooks.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
-
 set -e
+cd "$(dirname "$0")"
 # This script expects a safe umask set
 
-content_pre="content_pre"
-content_post="content_post"
-metadata_pre="metadata_pre"
-metadata_post="metadata_post"
-
+pre="pre"
+post="post"
 hook_type=$1
-
-if [[ "$hook_type" != "$content_pre" ]] \
-  && [[ "$hook_type" != "$content_post" ]] \
-  && [[ "$hook_type" != "$metadata_pre" ]] \
-  && [[ "$hook_type" != "$metadata_post" ]]
-then
+if [[ "$hook_type" != "$pre" ]] && [[ "$hook_type" != "$post" ]]; then
   echo "Bad hook type: [$hook_type]"
   exit 1
 fi
-
 
 ensure_unmounted() {
   disk=$1
@@ -30,67 +21,70 @@ ensure_unmounted() {
   fi
 }
 
+windows_disks_file="config/windows_disks.cfg"
+base_dir="/mnt/borg_windows"
 
-windows_disks_file="windows_disks.cfg"
-base_content_mnt="/mnt/borg_windows_content"
-base_metadata_dir="/tmp/borg_windows_metadata"
-
-if [[ "$hook_type" == "$metadata_pre" ]]; then
-  if [[ -e "$base_metadata_dir" ]]; then
-    echo "Error: base_metadata_dir ($base_metadata_dir) already exists"
+if [[ "$hook_type" == "$pre" ]]; then
+  if [[ -e "$base_dir" ]]; then
+    echo "Error: base_dir ($base_dir) already exists"
     exit 1
   fi
 
-  mkdir -p "$base_metadata_dir"
+  mkdir "$base_dir"
 fi
 
 
 if [[ ! -f $windows_disks_file ]]; then
-  echo $windows_disks_file doesnt exist
+  echo "$windows_disks_file doesn't exist"
   exit 1
 elif [[ ! -s $windows_disks_file ]]; then
-  echo $windows_disks_file is empty
+  echo "$windows_disks_file is empty"
   exit 1
 elif [[ ! -r $windows_disks_file ]]; then
-  echo $windows_disks_file is not readable
+  echo "$windows_disks_file is not readable"
   exit 1
 fi
 # Let's hope its format is correct
 
 while read -r disk dev_path; do
 
-  if [[ "$hook_type" == "$content_pre" ]]; then
+  mnt_path="$base_dir/$disk"
+  pipe_path="$base_dir/$disk.metadata.simg"
+
+  if [[ "$hook_type" == "$pre" ]]; then
     ensure_unmounted "$disk" "$dev_path"
-    mkdir -p "$base_content_mnt/$disk"
-    mount -o ro "$dev_path" "$base_content_mnt/$disk"
-
-  elif [[ "$hook_type" == "$content_post" ]]; then
-    umount "$base_content_mnt/$disk"
-    rmdir "$base_content_mnt/$disk"
-
-  elif [[ "$hook_type" == "$metadata_pre" ]]; then
-    ensure_unmounted "$disk" "$dev_path"
-    mkfifo "$base_metadata_dir/$disk"
-
+    
+    mkdir -p "$mnt_path"
+    mount -o ro "$dev_path" "$mnt_path"
+    # Optional:
+    problematic_files=$(find -L "$mnt_path" -type b -o -type c -o -type p -exec ls -lh {} + || true)
+    if [[ "$problematic_files" ]]; then
+      echo -e "\nFound the following files that can cause problems with --read-special:"
+      echo "$problematic_files"
+      exit 1
+    fi
+    
+    mkfifo "$pipe_path"
     ntfsclone \
       --metadata --preserve-timestamps \
       --save-image \
       --output - \
       "$dev_path" \
-      > "$base_metadata_dir/$disk" &
+      > "$pipe_path" &
 
-  elif [[ "$hook_type" == "$metadata_post" ]]; then
-    rm "$base_metadata_dir/$disk"
+  elif [[ "$hook_type" == "$post" ]]; then
+    umount "$mnt_path"
+    rmdir "$mnt_path"
+
+    rm "$pipe_path"
 
   else
     echo "hook type assertion failed"
     exit 1
   fi
-done < windows_disks_file.cfg
+done < $windows_disks_file
 
 
-if [[ "$hook_type" == "$content_post" ]]; then
-  rmdir "$base_content_mnt"
-elif [[ "$hook_type" == "$metadata_post" ]]; then
-  rmdir "$base_metadata_dir"
+if [[ "$hook_type" == "$post" ]]; then
+  rmdir "$base_dir"
 fi
