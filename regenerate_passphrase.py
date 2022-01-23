dbcf9495bfc4264f2be9e4f3bb3c4e20bc92df07
zzdroide <196064264+zzdroide@users.noreply.github.com>
2022-01-23 13:29:43 -0300

    chore: remove old ubuntu workaround

--- regenerate_passphrase.py
+++ regenerate_passphrase.py
@@ -4,5 +4,4 @@ readline.set_auto_history(False)
 
 
 from hashlib import scrypt
-# If import fails, try running in: docker run -it --rm python:3-buster
 scrypt(b'pass', salt=b'', n=2**20, r=8, p=1, maxmem=2**30+2**20, dklen=16).hex()
