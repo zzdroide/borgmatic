748c9d5e5da32f1dd5fe6ed4183d76e6b04250e7
zzdroide <196064264+zzdroide@users.noreply.github.com>
2022-03-05 18:34:01 -0300

    chore: note that problem may be specific to ntfs-3g

--- parts_hooks.sh
+++ parts_hooks.sh
@@ -82,8 +82,9 @@ cat $PARTS_CONFIG | while read -r part dev ntfs; do
       mkdir -p "$mnt_path"
       mount -o ro "$realdev" "$mnt_path"
 
-      # Windows Vista and higher seem to create weird files that appear as a pipe
+      # Windows Vista and higher seem to create weird files that appear as a pipe with ntfs-3g
       find -L "$mnt_path" -type b -o -type c -o -type p >> $NTFS_EXCLUDES 2> /dev/null || true  # TODO: "pf:" ?
+      # future: still a problem with paragon's ntfs3?
 
       mkfifo "$pipe_path"
       ntfsclone \
