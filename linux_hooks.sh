10c3ba1e59879b33e176d6b81ced733a78ee764e
zzdroide <196064264+zzdroide@users.noreply.github.com>
2022-09-29 21:54:27 -0300

    feat: autodiscover lvm snapshot params

--- linux_hooks.sh
+++ linux_hooks.sh
@@ -7,14 +7,8 @@ cd "$(dirname "$0")"
 readonly HOOK_TYPE=$1
 source shared/hooks.sh
 
-# Improvement: autodiscover or make configurable
-readonly VG="myvg"
-readonly LV="mylv"
-readonly SNAP_SIZE="2G"
-
-readonly MNT_DIR="/mnt/borg_lvm"
-readonly SNAP_NAME="borg_${LV}_snapshot"
-readonly SNAP_DEV="/dev/$VG/$SNAP_NAME"
+readonly MNT_DIR="/mnt/borg_root_snapshot"
+readonly SNAP_NAME="borg_snapshot"
 
 
 prune_docker_images() {
@@ -31,37 +25,43 @@ run_virtualbox_before_hook() {
   fi
 }
 
-make_and_mount_lvsnapshot() {
+make_and_mount_snapshot() {
+  local root_dev=$1 snap_dev=$2
+
   mkdir $MNT_DIR
-  lvcreate --size=$SNAP_SIZE --snapshot --permission=r --name=$SNAP_NAME /dev/$VG/$LV
-  mount -o ro $SNAP_DEV $MNT_DIR
+  lvcreate -qq --snapshot --extents=100%FREE --permission=r --name=$SNAP_NAME "$root_dev"
+  mount -o ro "$snap_dev" $MNT_DIR
 
   # Note: linux backup is not run first (so 02_linux.yaml) because on the first run,
   # chunks cache synchronization occurs, and would consume snapshot space.
 }
 
-umount_and_remove_lvsnapshot() {
+umount_and_remove_snapshot() {
+  local root_dev=$1 snap_dev=$2
+
   if findmnt $MNT_DIR >/dev/null; then
     umount $MNT_DIR
     sleep 2   # Otherwise "Logical volume xxxx contains a filesystem in use."
   fi
 
-  if lvdisplay $SNAP_DEV &>/dev/null; then
+  if lvdisplay "$snap_dev" &>/dev/null; then
     local percent rounded
-    percent=$(lvs --noheadings -o snap_percent $SNAP_DEV)
+    percent=$(lvs --noheadings -o snap_percent "$snap_dev")
     rounded=$(echo "$percent" | awk '{print int($1+0.5)}')
     echo "Snapshot usage: $rounded%"
-    lvremove --yes $SNAP_DEV
+    lvremove -qq --yes "$snap_dev"
   fi
 
   [[ ! -e $MNT_DIR ]] || rmdir $MNT_DIR
 }
 
 run_hook_before() {
+  local root_dev=$1 snap_dev=$2
+
   $0 $CLEANUP
   prune_docker_images
   run_virtualbox_before_hook
-  make_and_mount_lvsnapshot
+  make_and_mount_snapshot "$root_dev" "$snap_dev"
 
   # TODO: run once per repo instead of each borgmatic config:
   if root_borg_dirs_exist; then
@@ -71,7 +71,9 @@ run_hook_before() {
 }
 
 run_hook_cleanup() {
-  umount_and_remove_lvsnapshot
+  local root_dev=$1 snap_dev=$2
+
+  umount_and_remove_snapshot "$root_dev" "$snap_dev"
 }
 
 run_hook_after() {
@@ -85,11 +87,19 @@ run_hook_after() {
   fi
 }
 
+main() {
+  local root_dev vg_name snap_dev
+  root_dev=$(findmnt --noheadings --output source /)
+  vg_name=$(lvs --noheadings -o vg_name "$root_dev" | xargs echo)
+  snap_dev="/dev/$vg_name/$SNAP_NAME"
 
-if [[ "$HOOK_TYPE" == "$BEFORE" ]]; then
-  run_hook_before
-elif [[ "$HOOK_TYPE" == "$CLEANUP" ]]; then
-  run_hook_cleanup
-elif [[ "$HOOK_TYPE" == "$AFTER" ]]; then
-  run_hook_after
-fi
+  if [[ "$HOOK_TYPE" == "$BEFORE" ]]; then
+    run_hook_before "$root_dev" "$snap_dev"
+  elif [[ "$HOOK_TYPE" == "$CLEANUP" ]]; then
+    run_hook_cleanup "$root_dev" "$snap_dev"
+  elif [[ "$HOOK_TYPE" == "$AFTER" ]]; then
+    run_hook_after
+  fi
+}
+
+main
