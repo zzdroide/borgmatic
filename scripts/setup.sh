30d20ea604ce0f95e989b90a6cf382796a2f9005
zzdroide <196064264+zzdroide@users.noreply.github.com>
2024-01-21 20:09:45 -0300

    fix: use system python instead

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,18 +6,14 @@ set -euo pipefail
   && pipx_apt= \
   || pipx_apt=pipx
 
-sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo add-apt-repository -y ppa:rapier1/hpnssh
 sudo apt update
 
 # shellcheck disable=SC2086
 sudo apt install -y \
-  `# Python 3.12` \
-  python3.12 python3-pip python3-venv \
-  \
   `# To build Borg  # https://borgbackup.readthedocs.io/en/2.0.0b7/installation.html#debian-ubuntu` \
-  python3.12 python3.12-dev libacl1-dev libacl1 libssl-dev liblz4-dev libzstd-dev libxxhash-dev \
-  build-essential pkg-config python3-pkgconfig libfuse3-dev fuse3 \
+  python3 python3-dev python3-pip python3-virtualenv libacl1-dev libacl1 libssl-dev liblz4-dev \
+  libzstd-dev libxxhash-dev build-essential pkg-config python3-pkgconfig libfuse3-dev fuse3 \
   \
   hpnssh-client \
   wakeonlan smartmontools jq $pipx_apt
@@ -27,11 +23,11 @@ if [[ -z "$pipx_apt" ]]; then
 fi
 
 sudo PIPX_HOME=/opt/pipx PIPX_BIN_DIR=/usr/local/bin \
-  pipx install 'borgbackup[pyfuse3]==2.0.0b7' --suffix 2 --python python3.12
+  pipx install 'borgbackup[pyfuse3]==2.0.0b7' --suffix 2
 # Exact version because betas can have breaking changes.
 # Future: use a PPA
 
 sudo PIPX_HOME=/opt/pipx PIPX_BIN_DIR=/usr/local/bin \
-  pipx install 'borgmatic==1.8.5' --suffix 2 --python python3.12
+  pipx install 'borgmatic==1.8.5' --suffix 2
 # Exact version because the project doesn't follow semver
 # (search for `BREAKING` in the changelog: https://projects.torsion.org/borgmatic-collective/borgmatic/src/branch/master/NEWS)
