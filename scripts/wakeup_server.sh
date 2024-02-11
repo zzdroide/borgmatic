579ae99f70fc3b1d8757b2d3b22c39ec07ae1c26
zzdroide <196064264+zzdroide@users.noreply.github.com>
2024-02-11 03:28:08 -0300

    chore: change awake wait

--- scripts/wakeup_server.sh
+++ scripts/wakeup_server.sh
@@ -6,14 +6,11 @@ server_mac=$2
 
 wakeonlan "$server_mac" >/dev/null
 
-# FIXME: check if server is awake with curl (install nginx on server)
-#   hpnssh \
-#     -oBatchMode=yes \
-#     -oConnectTimeout=1 \
-#     -oConnectionAttempts=20 \
-#     "borg@$server_ip" \
-#     "borg --version"
-# Checking with ssh:
-# - is too slow (command must be "borg" and 2 hooks have to be called)
-# - [obsolete] The consecutive sshs (check awake, borg) fail because the second open_session
-#   runs before the first close_session finishes
+# Wait until awake
+curl \
+  -fsS \
+  --max-time 1 \
+  --retry 20 \
+  --retry-delay 1 \
+  --retry-connrefused \
+  "$server_ip"
