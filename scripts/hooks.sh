6db713c8c5e7d0db4f651eb8f7f3421b678f1f8e
zzdroide <196064264+zzdroide@users.noreply.github.com>
2025-07-10 03:20:36 -0400

    style: compact case

--- scripts/hooks.sh
+++ scripts/hooks.sh
@@ -10,18 +10,9 @@ source ../config/env
 source helpers/common.py
 
 case "$1" in
-  "$hook_before")
-    order_arg=""
-    ;;
-
-  "$hook_after"|"$hook_cleanup")
-    order_arg="--reverse"
-    ;;
-
-  *)
-    echo "Error: invalid hook type $1"
-    exit 1
-    ;;
+  "$hook_before") order_arg="" ;;
+  "$hook_after"|"$hook_cleanup") order_arg="--reverse" ;;
+  *) echo "Error: invalid hook type $1"; exit 1 ;;
 esac
 
 run-parts --regex="." --report $order_arg --exit-on-error --umask="$(umask)" --arg="$1" .
