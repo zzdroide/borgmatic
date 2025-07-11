78490112f68f332125f009b399e183e228025013
zzdroide <196064264+zzdroide@users.noreply.github.com>
2025-07-11 19:41:52 -0400

    feat: install specialfile

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,32 @@
 set -euo pipefail
 cd "$(dirname "$0")/../"
 
+
+install_specialfile() {
+  local tmpdir; tmpdir=$(mktemp --directory --tmpdir specialfile.XXXX)
+  pushd "$tmpdir" >/dev/null
+
+  git clone --depth 1 https://github.com/zzdroide/specialfile .
+  make
+  sudo make install
+
+  popd >/dev/null
+  rm -rf "$tmpdir"
+}
+
+keyscan_server() {
+  # This is like "ssh-keyscan {server_ip} >>~/.ssh/known_hosts"
+  # but using borgmatic to obtain {server_ip}.
+  borgmatic --verbosity=-2 \
+    --override ssh_command="hpnssh \
+      -oStrictHostKeyChecking=accept-new `# Automatically add to known_hosts` \
+      -oBatchMode=yes
+      -oPreferredAuthentications=null `# Fail authentication on purpose and close connection`" \
+    info 2>/dev/null
+}
+
+
+
 if (source /etc/os-release && [[
   "$VERSION_CODENAME" == "focal" || "$UBUNTU_CODENAME" == "focal"
 ]]); then
@@ -13,8 +39,6 @@ fi
 sudo add-apt-repository -y ppa:rapier1/hpnssh
 sudo add-apt-repository -y ppa:costamagnagianfranco/borgbackup
 sudo apt update
-
-# shellcheck disable=SC2086
 sudo apt install -y \
   borgbackup \
   hpnssh-client \
@@ -24,18 +48,8 @@ sudo apt install -y \
   pipx \
   libfuse-dev # To build specialfile
 
-# TODO: install specialfile
-#   make
-#   sudo make install
-
 sudo pipx install --global borgmatic==2.0.7
 # Exact version because the project doesn't follow semver  https://torsion.org/borgmatic/docs/how-to/upgrade/#versioning-and-breaking-changes
 
-# This is like "ssh-keyscan {server_ip} >>~/.ssh/known_hosts"
-# but using borgmatic to obtain {server_ip}.
-borgmatic --verbosity=-2 \
-  --override ssh_command="hpnssh \
-    -oStrictHostKeyChecking=accept-new `# Automatically add to known_hosts` \
-    -oBatchMode=yes
-    -oPreferredAuthentications=null `# Fail authentication on purpose and close connection`" \
-  info 2>/dev/null
+keyscan_server
+install_specialfile
