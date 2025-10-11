792ccf4d763a2535137e2fc1032c29bb45e31051
zzdroide <196064264+zzdroide@users.noreply.github.com>
2025-10-11 03:45:35 -0300

    fix: borg security path

--- hooks.d/10_chown_home.sh
+++ hooks.d/10_chown_home.sh
@@ -5,7 +5,7 @@ source helpers/common.py
 case "$1" in
   "$hook_cleanup")
     chown -Rf "$SUDO_USER:$SUDO_USER" \
-      "/home/$SUDO_USER/.local/share/borg/security/$REPO_ID" \
+      "/home/$SUDO_USER/.config/borg/security/$REPO_ID" \
       "/home/$SUDO_USER/.cache/borg/$REPO_ID" || true
     ;;
 esac
