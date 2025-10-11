e1644e578715bb9ac58db3ebca80040df5d08f3c
zzdroide <196064264+zzdroide@users.noreply.github.com>
2025-10-11 03:47:06 -0300

    fix: add repo to rename command

--- hooks.d/20_temp_archive.sh
+++ hooks.d/20_temp_archive.sh
@@ -9,6 +9,6 @@ case "$1" in
     ;;
   "$hook_after")
     # Only after all other $hook_after hooks have succeeded, mark this archive as valid.
-    borgmatic borg -- rename "${SERVER_USER}[temp]" "${SERVER_USER}-$(date +"%Y-%m-%d_%H:%M")"
+    borgmatic borg -- rename "::${SERVER_USER}[temp]" "${SERVER_USER}-$(date +"%Y-%m-%d_%H:%M")"
     ;;
 esac
