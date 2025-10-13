de0115b5e34f8a13086b45389aade955f0b4a9bf
zzdroide <196064264+zzdroide@users.noreply.github.com>
2025-10-13 00:21:11 -0300

    chore: improve borgmatic commands run by hooks

--- hooks.d/20_temp_archive.sh
+++ hooks.d/20_temp_archive.sh
@@ -2,13 +2,20 @@
 set -euo pipefail
 source helpers/common.py
 
+common_args="\
+  --verbosity=-1 `# verbosity=0 prints a "Deleting archives" line` \
+  --commands=[]"  # Don't run wakeup_server and others again
+
 case "$1" in
   "$hook_before")
     # For simplicity, have at most one temporary archive.
-    borgmatic --verbosity=0 delete --archive="${SERVER_USER}(temp)"
+    # shellcheck disable=SC2086
+    borgmatic $common_args delete --archive="${SERVER_USER}(temp)"
     ;;
+
   "$hook_after")
     # Only after all other $hook_after hooks have succeeded, mark this archive as valid.
-    borgmatic --verbosity=0 borg -- rename "::${SERVER_USER}(temp)" "${SERVER_USER}-$(date +"%Y-%m-%d_%H:%M")"
+    # shellcheck disable=SC2086
+    borgmatic $common_args borg -- rename "::${SERVER_USER}(temp)" "${SERVER_USER}-$(date +"%Y-%m-%d_%H:%M")"
     ;;
 esac
