244217e9627492a3c66892b1d43645841722b6c9
zzdroide <196064264+zzdroide@users.noreply.github.com>
2025-10-16 00:23:47 -0300

    feat: handle double /boot mount

--- hooks.d/31_pre_mounts.sh
+++ hooks.d/31_pre_mounts.sh
@@ -5,12 +5,29 @@ source helpers/common.py
 # Perform checks here to fail early, without wasting time in other expensive before hooks.
 
 unmount_boot_parts() {
+  local boot_dev; boot_dev=$(findmnt --noheadings --output=SOURCE /boot | sed 's|\[.*\]||' || true)
+
+  if [[ -z "$boot_dev" ]]; then
+    # /boot unmounted?
+    return 0
+  elif [[ "$boot_dev" == "$(findmnt --noheadings --output=SOURCE /)" ]]; then
+    # No separate boot partition
+    return 0
+  fi
+
   # "is-enabled" also checks for existence
   if systemctl is-enabled boot.mount &>/dev/null; then
     systemctl stop boot.mount
   fi
   if systemctl is-enabled boot-efi.mount &>/dev/null; then
     systemctl stop boot-efi.mount
+  fi
+
+  if findmnt "$boot_dev" >/dev/null; then
+    # Still mounted. This happens to me on Armbian, in which the partition is mounted at /media/mmcboot,
+    # and then /boot is a bind mount to /media/mmcboot/boot.
+    umount "$boot_dev"
+    # Fortunately nothing has to be done on remount, as "sc-start boot.mount" automatically mounts the dependency.
   fi
 }
 
