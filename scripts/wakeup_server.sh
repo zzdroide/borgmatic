44c60e71057ccb0373960d8217faa113c05ae2be
zzdroide <196064264+zzdroide@users.noreply.github.com>
2025-10-16 01:04:27 -0300

    fix: WOL with many networks

--- scripts/wakeup_server.sh
+++ scripts/wakeup_server.sh
@@ -4,7 +4,12 @@ set -euo pipefail
 server_ip=$1
 server_mac=$2
 
-wakeonlan "$server_mac" >/dev/null
+# Fixes WOL for a server infested with docker networks.
+# Assumes a /24 netmask.
+# Example: server_ip = 10.20.30.40 --> broadcast_ip = 10.20.30.255
+broadcast_ip="${server_ip%.*}.255"
+
+wakeonlan -i "$broadcast_ip" "$server_mac" >/dev/null
 
 # Wait until awake
 curl \
