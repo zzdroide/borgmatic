1ed0a2a3854827be88835435ffcd2c415339f34b
zzdroide <196064264+zzdroide@users.noreply.github.com>
2026-01-04 22:44:13 -0300

    feat: add ssh timeout in case server hangs

--- scripts/ssh_command.sh
+++ scripts/ssh_command.sh
@@ -7,9 +7,12 @@ filter_stderr() {
   grep -v "WARNING: ENABLED NONE CIPHER!!!"
 }
 
-exec sudo -u "${SUDO_USER:-$USER}" \
+sudo -u "${SUDO_USER:-$USER}" \
   SSH_AUTH_SOCK="$SSH_AUTH_SOCK" `# Provides unlocked identity_file` \
-  hpnssh -p1701 -oBatchMode=yes -oNoneEnabled=yes -oNoneSwitch=yes "$@" \
+  hpnssh \
+    -oBatchMode=yes -oNoneEnabled=yes -oNoneSwitch=yes \
+    -oServerAliveInterval=30 -oServerAliveCountMax=2 \
+    -p1701 "$@" \
 2> >(filter_stderr >&2)
 
 # https://unix.stackexchange.com/questions/3514/how-to-grep-standard-error-stream-stderr/3540#3540
