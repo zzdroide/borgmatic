a7a9f4c9a98ed2ad9b0eb213ff52c26e04ad7166
zzdroide <196064264+zzdroide@users.noreply.github.com>
2026-01-30 00:20:59 -0300

    fix: use multiple temp archives per user

--- hooks.d/20_temp_archive.sh
+++ hooks.d/20_temp_archive.sh
@@ -5,11 +5,7 @@ source helpers/common.py
 server_user=$(yq .server_user /etc/borgmatic/config/constants.yaml)
 
 # All `borgmatic` commands here use --commands="[]", otherwise this hook would recursively run again.
-
-delete_temp() {
-  borgmatic --commands="[]" --verbosity=-1 `# verbosity=0 prints a "Deleting archives" line` \
-    delete --archive="$server_user(temp)"
-}
+# Also, some commands use --verbosity=1 to prevent printing a "Running arbitrary Borg command" line.
 
 grep_chunks_cache_lines() {
   awk '/Synchronizing chunks cache\.\.\./{show=1} show; /Done\./{exit}'
@@ -18,19 +14,29 @@ grep_chunks_cache_lines() {
 
 case "$1" in
   "$hook_before")
-    # For simplicity, have at most one temporary archive.
-    delete_temp
-
     # Create a dummy temp archive to sync chunks cache now. This way it doesn't fill the snapshot later.
     borgmatic --commands="[]" --verbosity=1 \
       --source-directories="[/nonexistent]" --no-source-directories-must-exist --healthchecks.states="[]" \
       create | grep_chunks_cache_lines
-    delete_temp
     ;;
 
   "$hook_after")
     # Only after all other $hook_after hooks have succeeded, mark this archive as valid.
-    borgmatic --commands="[]" --verbosity=-1 `# verbosity=0 prints a "Running arbitrary Borg command" line` \
-      borg -- rename "::$server_user(temp)" "$server_user-$(date +"%Y-%m-%d_%H:%M")"
+
+    latest_temp_archive=$(borgmatic --commands="[]" --verbosity=0 \
+      list --short --glob-archives="$server_user(temp)-*" | tail -n1)
+
+    borgmatic --commands="[]" --verbosity=-1 \
+      borg -- rename "::$latest_temp_archive" "${latest_temp_archive/(temp)/}"
+
+    # Note: having a single temp archive per $server_user is simpler (just "$server_user(temp)"),
+    # but it has to be deleted before running `borgmatic create`, and that's a problem.
+    #
+    # Details:
+    # Deletion didn't take too long over LAN, but over higher latency internet it was too much.
+    # `borg delete` sends a lot of requests to the server: one per chunk referenced in the archive,
+    # see https://github.com/borgbackup/borg/blob/509c569/src/borg/archive.py#L1033.
+    # Even if fetch_async_response is called with wait=False,
+    # the SSH buffer fills up and the client blocks until the server catches up.
     ;;
 esac
